a9f0859f64e0125924dd087befdff4c4aa56228d
chamodya <chamodyarajapaksha1@gmail.com>
2026-01-17 08:12:16 +0530

    added basic version of project

---
+++ patient-service/src/main/java/com/pm/patient_service/PatientServiceApplication.java
@@ -0,0 +1,16 @@
+package com.pm.patient_service;
+
+import com.pm.patient_service.model.Patient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientServiceApplication.class, args);
+
+	}
+
+}
+
---
+++ patient-service/src/main/java/com/pm/patient_service/model/Patient.java
@@ -0,0 +1,45 @@
+package com.pm.patient_service.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+
+@Entity
+@Data                     // Generates getters, setters, toString, equals, hashCode
+@NoArgsConstructor          // Required by JPA
+@AllArgsConstructor
+
+public class Patient {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
+
+}
