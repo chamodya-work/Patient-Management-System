907247a74016696f228a6c7d41bb3e77850ec886
chamodya <chamodyarajapaksha1@gmail.com>
2026-01-17 11:02:32 +0530

    controller created

--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -4,9 +4,10 @@ import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class PatientService {
     private PatientRepository patientRepository;
 
