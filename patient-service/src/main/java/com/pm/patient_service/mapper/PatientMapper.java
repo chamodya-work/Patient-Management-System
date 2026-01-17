d7357e474b94c07501023167a6629ce2bed676b7
chamodya <chamodyarajapaksha1@gmail.com>
2026-01-17 12:03:14 +0530

    tested post endpoint

--- patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.pm.patient_service.mapper;
 
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
@@ -14,4 +17,14 @@ public class PatientMapper {
 
         return patientDTO;
     }
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
+    }
+
 }
