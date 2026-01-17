0a76b178982c8ab07844e4de783d83201c5ff531
chamodya <chamodyarajapaksha1@gmail.com>
2026-01-17 10:12:56 +0530

    service created

---
+++ patient-service/src/main/java/com/pm/patient_service/dto/PatientResponseDTO.java
@@ -0,0 +1,16 @@
+package com.pm.patient_service.dto;
+
+import lombok.Data;
+
+
+@Data
+
+public class PatientResponseDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private String dateOfBirth;
+
+
+}
